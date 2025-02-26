3c493da75e8ecdf64b58ea75faec567bd7d9373b
msaakaash <msaakaash@hotmail.com>
2025-02-27 00:04:47 +0530

    version 2.0

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
-function toggleMobileMenu(){
-    document.getElementById("menu").classList.toggle("active");
+function toggleMobileMenu() {
+    const menu = document.getElementById("menu");
+    menu.classList.toggle("active");
 }
 
